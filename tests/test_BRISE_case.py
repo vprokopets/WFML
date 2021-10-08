5b3760be3b3e7adf2e9b1294d15719d1d3de90a7
vprokopets <vprokopets95@gmail.com>
2021-10-08 17:53:14 +0200

    Update test

--- tests/test_BRISE_case.py
+++ tests/test_BRISE_case.py
@@ -3,11 +3,15 @@
 from os.path import dirname, abspath
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import FirefoxOptions
 
 
 class TestBRISEUPD():
     def setup_method(self):
-        self.driver = webdriver.Firefox(executable_path=f'{dirname(dirname(abspath(__file__)))}/geckodriver')
+        opts = FirefoxOptions()
+        opts.add_argument("--headless")
+        self.driver = webdriver.Firefox(options=opts,
+                                        executable_path=f'{dirname(dirname(abspath(__file__)))}/geckodriver')
         self.vars = {}
 
     def teardown_method(self):
