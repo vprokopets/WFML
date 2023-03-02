6c999bd64c9880086baa724688da44e97a642348
Volodymyr Prokopets <vprokopets95@gmail.com>
2023-03-02 21:37:15 +0100

    Release 0.2.0-alpha (#8)
    The major remake of WFML core & UI:
    
    * Dynamic constraint validation sequence
    * Dynamic step definition regardless to feature type
    * New namespace
    * New feature mapping scheme
    * New feature activation/deactivation scheme
    * Refactoring
    * Front-end update

--- ui/settings/urls.py
+++ ui/settings/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.shortcuts import redirect
+from django.urls import include, path, re_path
+
+def redirect_to_homepage(request):
+    response = redirect('/wizard/initialize/')
+    return response
+
 
 urlpatterns = [
     path('wizard/', include('ui.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^', redirect_to_homepage)
 ]
