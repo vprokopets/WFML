5f91ebc2f14359f0d06808c5ec3590059775cf0b
qx4mx <qx4mx1@gmail.com>
2023-04-24 17:15:25 +0200

    adjust models

--- ui/views.py
+++ ui/views.py
@@ -500,63 +500,76 @@ def download_modelica_file(request):
 # TODO seperate models to different files or move to frontend
 dpn_model = '\
 DPN{\
-    Places {\
-        PD * {\
-            startTokens -> integer\
-            nIn -> integer\
-            nOut -> integer\
-        }\
+  Places {\
+    PD* {\
+      startTokens-> integer\
+      nIn -> integer\
+      nOut -> integer\
+      [nIn >= 0]\
+      [nOut >= 0]\
     }\
-    Transitions {\
-        T * {\
-            nIn -> integer\
-            nOut -> integer\
-        }\
+  }\
+  Transitions {\
+    T* {\
+      nIn -> integer\
+      nOut -> integer\
+      [nIn >= 0]\
+      [nOut >= 0]\
     }\
-    Arcs {\
-        Arc*{\
-            start -> string\
-            end -> string\
-            [start in childs.DPN.Places or start in childs.DPN.Transitions]\
-            [end in childs.DPN.Places or end in childs.DPN.Transitions]\
-            [start != end]\
-        }\
+  }\
+  Arcs {\
+    Arc*{\
+      start -> string\
+      end -> string\
+      [start in childs.DPN.Places or start in childs.DPN.Transitions]\
+      [end in childs.DPN.Places or end in childs.DPN.Transitions]\
+      [start != end]\
     }\
+  }\
 }'
 
-hpn_model = '\
+hpn_model = "\
 HPN{\
-    Places {\
-        PD* {\
-			startTokens-> integer\
-			nIn -> integer\
-			nOut -> integer\
-        }\
-        PC* {\
-            startMarks -> float\
-            nIn -> integer\
-			nOut -> integer\
-        }\
+  Places {\
+    PD* {\
+      startTokens-> integer\
+      nIn -> integer\
+      nOut -> integer\
+      [nIn >= 0]\
+      [nOut >= 0]\
     }\
-    Transitions {\
-        TD* {\
-            nIn -> integer\
-            nOut -> integer\
-            delay -> float\
-        }\
-        TC*{\
-            nIn -> integer\
-            nOut -> integer\
-            maximumSpeed -> float\
-        }\
+    PC* {\
+      startMarks -> float\
+      nIn -> integer\
+      nOut -> integer\
+      [nIn >= 0]\
+      [nOut >= 0]\
     }\
-    Arcs {\
-        Arc*{\
-            start -> string\
-            end-> string\
-            [start in childs.HPN.Places or start in childs.HPN.Transitions ]\
-            [end in childs.HPN.Places or end in childs.HPN.Transitions ]\
-            [start != end]\
-        }\
+  }\
+  Transitions {\
+    T* {\
+      nIn -> integer\
+      nOut -> integer\
+      [nIn >= 0]\
+      [nOut >= 0]\
     }\
-}'
+    TC*{\
+      nIn -> integer\
+      nOut -> integer\
+      maximumSpeed -> float\
+      [nIn >= 0]\
+      [nOut >= 0]\
+    }\
+  }\
+  Arcs {\
+    Arc*{\
+      start -> string\
+      end-> string\
+      [start in childs.HPN.Places or start in childs.HPN.Transitions]\
+      [end in childs.HPN.Places or end in childs.HPN.Transitions]\
+      [start != end]\
+      [if start in childs.HPN.Places.PD then end not in childs.HPN.Transitions.TC]\
+      [if start in childs.HPN.Transitions.TC then end not in childs.HPN.Places.PD]\
+    }\
+  }\
+}"
