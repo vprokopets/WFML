99905a2cfa361753820c51b9ec0f937d49300cdd
qx4mx <qx4mx1@gmail.com>
2023-07-27 13:04:38 +0200

    reformat models, and apply naming workaroudn

--- ui/pn_models.py
+++ ui/pn_models.py
@@ -1,75 +1,79 @@
 class PetriNetModels:
-    dpn_model = '\
-DPN{\
-  Places {\
-    PD* {\
-      startTokens-> integer\
-      nIn -> integer\
-      nOut -> integer\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-  }\
-  Transitions {\
-    T* {\
-      nIn -> integer\
-      nOut -> integer\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-  }\
-  Arcs {\
-    Arc*{\
-      start -> string\
-      end -> string\
-      [start in childs.DPN.Places or start in childs.DPN.Transitions]\
-      [end in childs.DPN.Places or end in childs.DPN.Transitions]\
-      [start != end]\
-    }\
-  }\
-}'
+    dpn_model = "\
+        DPN{\
+            Places {\
+                PD* {\
+                    startTokens-> integer\
+                    nIn -> integer\
+                    nOut -> integer\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                }\
+            }\
+            Transitions {\
+                T* {\
+                    nIn -> integer\
+                    nOut -> integer\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                }\
+            }\
+            Arcs {\
+                Arc*{\
+                    start -> string\
+                    end -> string\
+                    [start in childs.DPN.Places or start in childs.DPN.Transitions]\
+                    [end in childs.DPN.Places or end in childs.DPN.Transitions]\
+                    [start != end]\
+                }\
+            }\
+        }\
+    "
     hpn_model = "\
-HPN{\
-  Places {\
-    PD* {\
-      startTokens-> integer\
-      nIn -> integer\
-      nOut -> integer\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-    PC* {\
-      startMarks -> float\
-      nIn -> integer\
-      nOut -> integer\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-  }\
-  Transitions {\
-    T* {\
-      nIn -> integer\
-      nOut -> integer\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-    TC*{\
-      nIn -> integer\
-      nOut -> integer\
-      maximumSpeed -> float\
-      [nIn >= 0]\
-      [nOut >= 0]\
-    }\
-  }\
-  Arcs {\
-    Arc*{\
-      start -> string\
-      end-> string\
-      [start in childs.HPN.Places or start in childs.HPN.Transitions]\
-      [end in childs.HPN.Places or end in childs.HPN.Transitions]\
-      [start != end]\
-      [if start in childs.HPN.Places.PD then end not in childs.HPN.Transitions.TC]\
-      [if start in childs.HPN.Transitions.TC then end not in childs.HPN.Places.PD]\
-    }\
-  }\
-}"
+        HPN{\
+            Places {\
+                PD* {\
+                    startTokens-> integer\
+                    nIn -> integer\
+                    nOut -> integer\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                }\
+                PC* {\
+                    startMarks -> float\
+                    nIn -> integer\
+                    nOut -> integer\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                }\
+            }\
+            Transitions {\
+                TD* {\
+                    nIn -> integer\
+                    nOut -> integer\
+                    delay -> integer\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                    [delay >= 0]\
+                }\
+                TC*{\
+                    nIn -> integer\
+                    nOut -> integer\
+                    maximumSpeed -> float\
+                    [nIn >= 0]\
+                    [nOut >= 0]\
+                }\
+            }\
+            Arcs {\
+                Arc*{\
+                    start -> string\
+                    end-> string\
+                    [start in childs.HPN.Places or start in childs.HPN.Transitions]\
+                    [end in childs.HPN.Places or end in childs.HPN.Transitions]\
+                    [start != end]\
+                    [if start in childs.HPN.Places.PD then end not in childs.HPN.Transitions.TC]\
+                    [if start in childs.HPN.Transitions.TC then end not in childs.HPN.Places.PD]\
+                }\
+            }\
+        }\
+    "
